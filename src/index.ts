fbed534444f20530081162d34eb056f4cc74d025
Kevindra Singh <kevindra.singh@gmail.com>
2020-10-22 02:52:53 -0700

    feat: version bump and plugin usage

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,30 @@
-import { alexa as ax, AlexaDialogContext, AlexaEvent, Intent, Locale } from "@chitchatjs/alexa";
-import builtins from "./blocks/builtins";
+import { alexa as ax, Locale } from "@chitchatjs/alexa";
+import { axkit } from "@chitchatjs/plugin-ax-kit";
+
 import artifacts from "./blocks/artifacts";
-import welcome from "./blocks/welcome";
-import requestSizeTemperament from "./blocks/requestslots";
 import confirm from "./blocks/confirm";
+import requestSizeTemperament from "./blocks/requestslots";
+import welcome from "./blocks/welcome";
+
+/**
+ * A plugin building block
+ * It will help us render experiences for builtin intents like
+ * - Stop, Cancel, Fallback etc.
+ */
+let builtins = axkit.builtin.all("You can tell me dog size or temperament to get started.", [
+  Locale.en_US,
+]);
 
 // welcome
 let init = ax
   .start()
-  .block(ax.compound().add(artifacts).add(welcome).add(builtins).add(ax.goto("PetMatcher")).build())
+  .block(ax.compound().add(artifacts).add(welcome).add(ax.goto("PetMatcher")).build())
   .build();
 
 // match a pet
 let petMatcher = ax
   .state("PetMatcher")
-  .block(ax.compound().add(requestSizeTemperament).add(confirm).add(builtins).add(ax.end()).build())
+  .block(ax.compound().add(requestSizeTemperament).add(confirm).add(builtins).build())
   .build();
 
 let skill = ax.skill().addState(init).addState(petMatcher).build();
